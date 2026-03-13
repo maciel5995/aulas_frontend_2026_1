bf8db5e6bc4d3acd1a6fd02892f42a9ad6e70953
Rodrigo Cesar Nunes Maciel <rodrigo.maciel@satc.edu.br>
2026-03-12 21:32:40 -0300

    códigos da aula 04

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+app.get("/cadastro",(req,res) => {
+    console.log("GET:", req.query);
+    res.send("Dados recebidos via GET");
+});
+app.post("/cadastro",(req,res) => {
+    console.log("POST:", req.body);
+    res.send("Dados recebidos via POST");
+});
+
+app.listen(3000,()=>{
+    console.log("Servidor rodando em http://localhost:3000");
+});
